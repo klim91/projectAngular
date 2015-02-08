609495a0ff9291f3365d0b7ca56518fe949d8d8e
klim91 <milosnik1991@o2.pl>
2015-02-08 12:16:07 +0000

    add file: js/videoLibrary.js

---
+++ js/videoLibrary.js
@@ -0,0 +1,7 @@
+angular.module("videoApp", ["firebase"])
+
+    .constant("FIREBASE_URI", "https://videolibrary-klim91.firebaseio.com/")
+    
+    .controller("videoCtrl", function($scope){
+        
+    })
