c4f19eba58599aa3744dd0363981a85f5f5701ab
klim91 <milosnik1991@o2.pl>
2015-02-08 15:02:14 +0000

    Add factory connect with firebase; Add functions for category: add, remove, save, get

--- js/videoLibrary.js
+++ js/videoLibrary.js
@@ -2,6 +2,49 @@ angular.module("videoApp", ["firebase"])
 
     .constant("FIREBASE_URI", "https://videolibrary-klim91.firebaseio.com/")
     
-    .controller("videoCtrl", function($scope){
+    .controller("videoCtrl",  function($scope, VideoService){
+        $scope.newCategory = {name: ''};
+        $scope.currentCategory = null;
+        
+        $scope.categories = VideoService.getCategories();
+        $scope.addCategory = function(category) {
+            VideoService.addCategory({description: category});
+        };
+        $scope.removeCategory = function(category){
+            VideoService.removeCategory(category);
+        };
+        $scope.updateCategory = function(category){
+            VideoService.updateCategory(category);
+        };
+        
+    })
+    
+    .factory("VideoService", function($firebase, FIREBASE_URI){
+        var refCategory = new Firebase(FIREBASE_URI);
+        var sync = $firebase(refCategory);
+        var categories = sync.$asArray();
+        
+        var getCategories = function(){
+            return categories;    
+        };
+        
+        var addCategory = function(category){
+            categories.$add(category);
+        };
+        
+        var removeCategory = function(category){
+            categories.$remove(category);   
+        };
+        
+        var updateCategory = function(category){
+            categories.$save(category);
+        };
+        
+        return {
+            getCategories: getCategories,
+            addCategory: addCategory,
+            removeCategory: removeCategory,
+            updateCategory: updateCategory
+        }
         
     })
