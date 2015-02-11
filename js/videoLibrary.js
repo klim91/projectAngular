c9a05919fe0159032ece94da952a91690bfbcfb6
klim91 <milosnik1991@o2.pl>
2015-02-11 19:50:14 +0000

    split on 3 files

--- js/videoLibrary.js
+++ js/videoLibrary.js
@@ -1,4 +1,4 @@
-angular.module("videoApp", ["firebase", "ui.router"])
+angular.module("videoApp", ["ui.router", "videoApp.factoryImageService", "videoApp.factoryBookmarkService"])
 
     .config(function($stateProvider, $urlRouterProvider) {
           $stateProvider
@@ -67,79 +67,4 @@ angular.module("videoApp", ["firebase", "ui.router"])
             $scope.currentBookmark = bookmark;
         };
         
-    })
-    
-    .factory("ImageService", function($firebase, FIREBASE_URI){
-        var refCategory = new Firebase(FIREBASE_URI);
-        var sync = $firebase(refCategory);
-        var categories = sync.$asArray();
-        
-        var getCategories = function(){
-            return categories;    
-        };
-        
-        var addCategory = function(category){
-            categories.$add(category);
-        };
-        
-        var removeCategory = function(category){
-            categories.$remove(category);   
-        };
-        
-        var updateCategory = function(category){
-            categories.$save(category);
-        };
-        
-        var isExistsCategory = function(category){
-            var len = categories.length;
-            for(var i=0; i<len; i++){
-                if(categories[i].name === category.name)
-                    return true;
-            }
-            return false;
-        };
-        
-        return {
-            getCategories: getCategories,
-            addCategory: addCategory,
-            removeCategory: removeCategory,
-            updateCategory: updateCategory,
-            isExistsCategory: isExistsCategory
-        }
-        
-    })
-    
-    .factory("BookmarksImageService", function($firebase, FIREBASE_URI){
-        var refBookmarks = new Firebase(FIREBASE_URI+"/bookmarks");
-        var sync = $firebase(refBookmarks);
-        var bookmarks = sync.$asArray();    
-        
-        var getBookmarks = function(){
-            return bookmarks;
-        };
-        var addBookmark = function(bookmark){
-            bookmarks.$add(bookmark);
-        };
-        var removeBookmark = function(bookmark){
-            bookmarks.$remove(bookmark);
-        };
-        var updateBookmark = function(bookmark){
-            bookmarks.$save(bookmark);    
-        };
-        var removeAllBookmarkForCategory = function(category){
-            var len = bookmarks.length;
-            for (var i=0; i<len; i++) {
-                if (bookmarks[i].categoryname === category.name) {
-                    bookmarks.$remove(i);
-                }
-            }
-        };
-
-        return {
-            getBookmarks: getBookmarks,
-            addBookmark: addBookmark,
-            removeBookmark: removeBookmark,
-            updateBookmark: updateBookmark,
-            removeAllBookmarkForCategory: removeAllBookmarkForCategory
-        }
-    })
+    });
